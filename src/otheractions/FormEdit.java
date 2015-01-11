575d965a032e45f1aef91aa21872e689d8a36f33
wertwerzb <wertwerzb@sina.com>
2015-01-11 23:51:42 +0800

    自动修改action

---
+++ src/otheractions/FormEdit.java
@@ -0,0 +1,200 @@
+package otheractions;
+
+import 
+android.os.Environment; 
+import java.io.*;
+import java.util.*;
+import org.dom4j.*;
+import org.dom4j.io.*;
+import java.sql.ResultSet;
+import java.net.URLDecoder;
+import util.innpath;
+import util.Idcode;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import Com.db.DruidConnect;
+import Com.util.StrFun;
+
+
+import java.sql.Connection;
+import java.sql.Statement;
+public class FormEdit
+{
+	private String  selectcolumn;
+	private 	String  fieldtitle; 
+	private 	String  sigleform;
+	private 	String  idfield ;
+	private 	String  rolestr ;
+	private 	String  typestr ;
+
+	private 	String path;
+	private 	String extendfield;
+private 	String modextfield;
+
+	private 	String usercode;
+private 	String filepath;
+	public FormEdit(String role, String czy, String hreft,String fileph )
+	{
+		rolestr = role;
+
+		usercode = czy;
+		 filepath= fileph;
+		setvar(hreft);
+
+	} 
+	
+	public String getmodsql(HttpServletRequest request) throws Exception
+	{
+		String sql = "Update " + sigleform + " set " ; 
+		HashMap<String,String> hm=new HashMap<String,String>(); 
+	 	String[] feildstr = new String[40];
+		String[] valuestr = new String[40];
+		feildstr = selectcolumn.split(",");
+		valuestr = fieldtitle.split(",");
+		String  idstr=" where " + feildstr[0] + "='" + StrFun.getString(request, valuestr[0]) + "'";
+	String[] dattype = new String[40];
+		dattype = typestr.split(",");
+
+		for (int i= 0; i < valuestr.length;i++)
+		{
+		 if(!("h".equals(dattype[i]))) 	hm.put(feildstr[i], "'"+java.net.URLDecoder.decode(request.getParameter( valuestr[i] ) )+"'");
+		}
+		 if (modextfield.length() > 0)
+		{
+			String[] modextstr = new String[40];
+    	modextstr = modextfield.split(",");
+			for (int i= 0; i < modextstr.length;i++)
+			{
+				try
+				{
+					if (("user").equals(modextstr[i].split(":")[1]))
+					 hm.put( modextstr[i].split(":")[0], "'"+ usercode+ "'" );
+	 			else if (("role").equals(modextstr[i].split(":")[1]))
+				 hm.put( modextstr[i].split(":")[0], "'"+ rolestr+ "'" );
+	 		 		else if (("getid").equals(modextstr[i].split(":")[1])) 
+	 		 	 hm.put( modextstr[i].split(":")[0], "'"+ Idcode.getidcode(idfield, modextstr[i].split(":")[2], sigleform, modextstr[i].split(":")[3]) + "'" );
+	 	 		else if (("ip").equals(modextstr[i].split(":")[1])) 
+	 		 	 hm.put( modextstr[i].split(":")[0], "'"+ request.getRemoteAddr()+ "'" );
+	 		 	 		
+	 			else 
+	 			 hm.put( modextstr[i].split(":")[0], modextstr[i].split(":")[1] );
+	 			}
+				catch (Exception e)
+				{}
+			}
+		}
+
+ java.util.Iterator it = hm.entrySet().iterator();
+   while(it.hasNext()){
+   java.util.Map.Entry entry = (java .util.Map.Entry) it.next();
+
+sql = sql + entry.getKey() + "="  + entry.getValue () + "," ;
+
+}
+
+sql = sql.substring(0, sql.length() - 1); 
+
+return replcerol( sql+ idstr );
+	
+	}
+	
+	
+	
+	public void Edit(HttpServletRequest request) throws Exception
+{
+//String str= getmodsql(request) ;
+String str= getmodsql( request );
+		  Connection conn = DruidConnect.openConnection(); 
+  Statement dbc = conn.createStatement(); 
+		dbc.executeUpdate(str);
+		dbc.close();
+		 conn.close();
+}
+	public void Del (HttpServletRequest request) throws Exception 
+	{
+		 String id = request.getParameter("id");
+		String Str="Delete From " + sigleform + " where " + idfield + "='" + id + "'" ;
+		//and bcp_Uflag=0";
+		  Connection conn = DruidConnect.openConnection(); 
+  Statement dbc = conn.createStatement(); 
+		dbc.executeUpdate(Str);
+		dbc.close();
+conn.close();
+	}	
+	public static String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		if (ip.equals("0:0:0:0:0:0:0:1")) {
+			ip = "本地";
+		}
+		if (ip.split(",").length > 1) {
+			ip = ip.split(",")[0];
+		}
+		return ip;
+	}
+	 private String replcerol(String sql )
+	{
+	String prole;
+	String resultstr= sql ;
+	if( rolestr.length()>1 )
+	 	prole= rolestr.substring( 0,rolestr.length()-2 ) ; 
+	else
+	 prole="";
+
+if( sql.indexOf("{R}")>0)
+resultstr= sql.replace("{R}", rolestr );
+
+ if( sql.indexOf("{U}")>0)
+resultstr= resultstr.replace("{U}", usercode);
+
+  return resultstr;
+	}
+
+	private void setvar(String hreft)
+	{
+
+
+		String 	  realPath= innpath.getxmlpath(rolestr, usercode, "/"+ filepath+ "/" + hreft + ".xml");
+		try
+		{
+			SAXReader reader = new SAXReader();
+			OutputFormat format = OutputFormat.createPrettyPrint();
+			format.setEncoding("UTF-8");
+			File file = new File(realPath);
+			if (file.isFile())
+			{
+				Document document = reader.read(file);
+				Element database = (Element)document.selectSingleNode("//" + hreft);
+				selectcolumn = database.element("selectcolumn").getText().replaceAll("\\s*", "") ;
+				fieldtitle = database.element("fieldtitle").getText().replaceAll("\\s*", "") ;
+				sigleform = database.element("sigleform").getText().trim() ;
+				idfield = database.element("idfield").getText().trim() ;
+				typestr = database.element("typestr").getText().trim() ;
+				extendfield = database.element("extendfield").getText().trim() ;
+      modextfield = database.element("modextfield").getText().trim() ;
+
+
+			}
+			else
+			{
+				System.out.println(realPath + " ,outer xml not exist");
+			}
+
+		}
+		catch (Exception ce)
+		{
+			ce.printStackTrace();
+		}
+	}
+}
